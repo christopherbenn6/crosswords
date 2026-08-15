f83a693235798c3d9c7473bfd726ac42f4d8971a
Christopher Benn <christopherbenn6@gmail.com>
2026-08-14 18:56:53 -0600

    Dev: Word validation via bubble checking and looping the process

--- crosswords/src/logic/crossword.js
+++ crosswords/src/logic/crossword.js
@@ -1,5 +1,5 @@
 import words from "../assets/words.json"
-console.log(words)
+
 /**
  * @param {int} difficulty - Interger from 1 to 3
  * @param {int} wordCount - 
@@ -9,83 +9,122 @@ export async function createCrosswordData(difficulty, wordCount) {
     const gridHeight = 30;
     const gridWidth = 30;
 
-    let crosswordState = {
-        words: [
-            {
-                word: "cheexe",
-                direction: "across",
-                letters: [
-                    {
-                        letter: "c",
-                        XPos: 7,
-                        YPos: 17
-                    },
-                    {
-                        letter: "h",
-                        XPos: 8,
-                        YPos: 17
-                    },
-                    {
-                        letter: "e",
-                        XPos: 9,
-                        YPos: 17
-                    },
-                    {
-                        letter: "e",
-                        XPos: 10,
-                        YPos: 17
-                    },
-                    {
-                        letter: "s",
-                        XPos: 11,
-                        YPos: 17
-                    },
-                    {
-                        letter: "e",
-                        XPos: 12,
-                        YPos: 17
-                    },
-                ] 
-            },
-            {
-                word: "ballx",
-                direction: "down",
-                letters: [
-                    {
-                        letter: "b",
-                        XPos: 11,
-                        YPos: 13
-                    },
-                    {
-                        letter: "a",
-                        XPos: 11,
-                        YPos: 14
-                    },
-                    {
-                        letter: "l",
-                        XPos: 11,
-                        YPos: 15
-                    },
-                    {
-                        letter: "l",
-                        XPos: 11,
-                        YPos: 16
-                    },
-                    {
-                        letter: "s",
-                        XPos: 11,
-                        YPos: 17
-                    }
-                ] 
-            }
-        ],
+    // let crosswordState = {
+    //     words: [
+    //         {
+    //             word: "cheexe",
+    //             direction: "across",
+    //             letters: [
+    //                 {
+    //                     letter: "c",
+    //                     XPos: 7,
+    //                     YPos: 17
+    //                 },
+    //                 {
+    //                     letter: "h",
+    //                     XPos: 8,
+    //                     YPos: 17
+    //                 },
+    //                 {
+    //                     letter: "e",
+    //                     XPos: 9,
+    //                     YPos: 17
+    //                 },
+    //                 {
+    //                     letter: "e",
+    //                     XPos: 10,
+    //                     YPos: 17
+    //                 },
+    //                 {
+    //                     letter: "s",
+    //                     XPos: 11,
+    //                     YPos: 17
+    //                 },
+    //                 {
+    //                     letter: "e",
+    //                     XPos: 12,
+    //                     YPos: 17
+    //                 },
+    //             ] 
+    //         },
+    //         {
+    //             word: "ballx",
+    //             direction: "down",
+    //             letters: [
+    //                 {
+    //                     letter: "b",
+    //                     XPos: 11,
+    //                     YPos: 13
+    //                 },
+    //                 {
+    //                     letter: "a",
+    //                     XPos: 11,
+    //                     YPos: 14
+    //                 },
+    //                 {
+    //                     letter: "l",
+    //                     XPos: 11,
+    //                     YPos: 15
+    //                 },
+    //                 {
+    //                     letter: "l",
+    //                     XPos: 11,
+    //                     YPos: 16
+    //                 },
+    //                 {
+    //                     letter: "s",
+    //                     XPos: 11,
+    //                     YPos: 17
+    //                 }
+    //             ] 
+    //         }
+    //     ],
 
+    // };
+
+    let crosswordState = {
+        words: [{
+                    word: "cheese",
+                    direction: "across",
+                    letters: [
+                        {
+                            letter: "c",
+                            XPos: 7,
+                            YPos: 17
+                        },
+                        {
+                            letter: "h",
+                            XPos: 8,
+                            YPos: 17
+                        },
+                        {
+                            letter: "e",
+                            XPos: 9,
+                            YPos: 17
+                        },
+                        {
+                            letter: "e",
+                            XPos: 10,
+                            YPos: 17
+                        },
+                        {
+                            letter: "s",
+                            XPos: 11,
+                            YPos: 17
+                        },
+                        {
+                            letter: "e",
+                            XPos: 12,
+                            YPos: 17
+                        },
+                    ] 
+                }]
     };
-    getRandomWordObject(difficulty, crosswordState.words);
-    // for(let i = wordCount; i > 0; i--) {
-    //     crosswordState.words += getRandomWordObject(difficulty, crosswordState.words);
-    // }
-    // console.log(crosswordState.words)
+    for(let i = wordCount - 1; i > 0; i--) {
+        crosswordState.words.push(getRandomWordObject(difficulty, crosswordState.words));
+    }
+    console.log(crosswordState)
+
 }
 
 /**
@@ -95,21 +134,36 @@ export async function createCrosswordData(difficulty, wordCount) {
  */
 function getRandomWordObject(difficulty, crosswordStateWords) {
     const allLetters = getAllLetterPositions(crosswordStateWords);
-    const randLetter = allLetters[getRandomRange(0, allLetters.length - 1)]
-    console.log(randLetter);
+    let newWord;
+    let wordAdded = false;
 
-    // Filter the words that contain the random letter
-    const wordsThatContainRandLetter = words.filter(wordObject =>
-    wordObject.word
-        .toLowerCase()
-        .includes(randLetter.letter.toLowerCase())
-    );
+    // Loop this until a valid word has been found
+    while(!wordAdded) {
+        const randLetter = allLetters[getRandomRange(0, allLetters.length - 1)]
+        console.log(randLetter);
 
-    let randWord = getRandomWordFromArray(wordsThatContainRandLetter);
-    let isVertical = checkIsVertical(crosswordStateWords, randLetter, allLetters);
+        // Filter the words that contain the random letter
+        const wordsThatContainRandLetter = words.filter(wordObject =>
+        wordObject.word
+            .toLowerCase()
+            .includes(randLetter.letter.toLowerCase())
+        );
 
-    let wordObject = createWordObject(randWord, randLetter, isVertical)
-    let isValid = isValidWord(wordObject, allLetters);
+        let randWord = getRandomWordFromArray(wordsThatContainRandLetter);
+        let isVertical = checkIsVertical(crosswordStateWords, randLetter, allLetters);
+
+        let wordObject = createWordObject(randWord, randLetter, isVertical)
+        let isValid = isValidWord(wordObject, allLetters, crosswordStateWords);
+
+        if(isValid) {
+            newWord = wordObject;
+            console.log("Added")
+            wordAdded = true;
+        }
+    }
+
+    return newWord;
+
 }
 
 function getAllLetterPositions(crosswordStateWords) {
@@ -237,7 +291,7 @@ function createWordObject(word, letter, isVertical) {
  * @param {object} letter - The random letter and location to place the word
  * @returns {boolean}
  */
-function isValidWord(word, allLetters) {
+function isValidWord(word, allLetters, crosswordStateWords) {
     // Check the position of every letter to see if it matches another location, but NOT the same letter
     let isValid = true;
 
@@ -254,5 +308,112 @@ function isValidWord(word, allLetters) {
         })
     });
 
-    
+    // Here we check a "bubble" around the word to check for anything side by side that would break the word or create new ones
+    if(word.direction == "across") {
+
+        // Check before and after the word
+        const leftXPos = word.letters[0].XPos - 1;
+        const leftYPos = word.letters[0].YPos;
+        const rightXPos = word.letters[word.letters.length - 1].XPos + 1;
+        const rightYPos = word.letters[word.letters.length - 1].YPos;
+
+        if(allLetters.find(
+            letterPosition =>
+                (letterPosition.XPos === rightXPos && letterPosition.YPos === rightYPos)
+                || (letterPosition.XPos === leftXPos && letterPosition.YPos === leftYPos)
+        )) {
+            isValid = false;
+        }
+
+        for (let i = 0; i < word.letters.length; i++) {
+            // Positions for above and below the current letter (word.letters[i])
+            const XPos = word.letters[i].XPos;
+            const upYPos = word.letters[i].YPos + 1;
+            const downYPos = word.letters[i].YPos - 1;
+
+            // Check for an existing letter there
+            const existingUpLetter = allLetters.find(
+            letterPosition =>
+                letterPosition.XPos === XPos && letterPosition.YPos === upYPos
+            );
+
+
+            const existingDownLetter = allLetters.find(
+            letterPosition =>
+                letterPosition.XPos === XPos && letterPosition.YPos === downYPos
+            );
+
+            // Is that letter okay to be there (ie: is it part of an existing word). 
+            // Check for both that letter and the current letter in the same word
+            if(existingDownLetter) {
+                if(!areLettersInSameWord(word.letters[i], existingDownLetter, crosswordStateWords)) {
+                    isValid = false;
+                    console.log(isValid)
+                }
+            } else if(existingUpLetter) {
+                if(!areLettersInSameWord(word.letters[i], existingUpLetter, crosswordStateWords)) {
+                    isValid = false;
+                    console.log(isValid)
+                }
+            }
+        }
+
+    } else if(word.direction == "down") {
+        // Check before and after the word
+        const upXPos = word.letters[0].XPos - 1;
+        const upYPos = word.letters[0].YPos;
+        const downXPos = word.letters[word.letters.length - 1].XPos + 1;
+        const downYPos = word.letters[word.letters.length - 1].YPos;
+
+        if(allLetters.find(
+            letterPosition =>
+                (letterPosition.XPos === upXPos && letterPosition.YPos === upYPos)
+                || (letterPosition.XPos === downXPos && letterPosition.YPos === downYPos)
+        )) {
+            isValid = false;
+        }
+
+        for (let i = 0; i < word.letters.length; i++) {
+            // Positions for above and below the current letter (word.letters[i])
+            const YPos = word.letters[i].YPos;
+            const leftXPos = word.letters[i].XPos - 1;
+            const rightXPos = word.letters[i].XPos + 1;
+
+            // Check for an existing letter there
+            const existingLeftLetter = allLetters.find(
+            letterPosition =>
+                letterPosition.YPos === YPos && letterPosition.XPos === leftXPos
+            );
+
+
+            const existingRightLetter = allLetters.find(
+            letterPosition =>
+                letterPosition.YPos === YPos && letterPosition.XPos === rightXPos
+            );
+
+            // Is that letter okay to be there (ie: is it part of an existing word). 
+            // Check for both that letter and the current letter in the same word
+            if(existingLeftLetter) {
+                if(!areLettersInSameWord(word.letters[i], existingLeftLetter, crosswordStateWords)) {
+                    isValid = false;
+                }
+            } else if(existingRightLetter) {
+                if(!areLettersInSameWord(word.letters[i], existingRightLetter, crosswordStateWords)) {
+                    isValid = false;
+                }
+            }
+        }
+    }
+
+    return isValid;
+
+}
+
+function areLettersInSameWord(x, y, crosswordStateWords) {
+    if(x === null || y === null) return false;
+
+    return crosswordStateWords.some(word => {
+        return word.letters.includes(x) &&
+            word.letters.includes(y);
+});
 }
